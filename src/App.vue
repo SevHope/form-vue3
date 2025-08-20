ca23e313361b0bba6fe911e2fcb6bf9b6ab334e6
Nadezhda Sevastianova <s.hope@bk.ru>
2025-08-20 10:07:58 +0300

    delete comments

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,6 @@
 <script setup>
-// import { reactive } from 'vue';
 import { useFormStore } from './stores/FormStore';
 import { MaskInput } from 'vue-3-mask';
-
-// const state = reactive({
-//   formValid: true,
-//   formStatus: 'active' //active-все доступно для редактирования, 'isSubmitting' - в процессе валидации, 'submited' - после валидации, форма заблокирована, на экране модалка
-// })
-
-// const personsData = reactive({
-//   initials: '',
-//   email: '',
-//   phone: '',
-//   date: '',
-//   country: '',
-//   index: '',
-//   checked: false,
-//   message: ''
-// })
-
-// const formItems = reactive([
-//   {id: 'initials', tag: 'input', class: 'formInput', placeholder: 'Введите ФИО', type: 'text', label: 'ФИО', required: true, minLength: '2', maxLength: '200', restrictions: /^[A-Za-zА-Яа-яёЁ\s-]+$/, errorMessage: 'ФИО должно быть от 2 до 200 символов и содержать только буквы, пробелы и дефис', showError: false },
-//   {id: 'email', tag: 'input', class: 'formInput', placeholder: 'Введите почту', type: 'email', label: 'Почта', required: true, restrictions: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+/, errorMessage: 'Введен некорректный email', showError: false },
-//   {id: 'phone', tag: 'input', class: 'formInput', placeholder: '+7 (xxx) xxx-xx-xx', type: 'tel', label: 'Телефон', required: true, correctLength: 11, restrictions: /\D/g, errorMessage: 'Укажите телефон в формате +7 xxx xxx-xx-xx', showError: false },
-//   {id: 'date', tag: 'input', class: 'formInput', placeholder: 'Введите дату рождения', type: 'date', label: 'Дата рождения', minAge: 18, errorMessage: 'Возраст должен быть больше или равен 18', showError: false },
-//   {id: 'country', tag: 'select', class: 'formSelect', placeholder: 'Укажите страну', label: 'Страна', showError: false},
-//   {id: 'index', tag: 'input', class: 'formInput', placeholder: 'Введите почтовый индекс', type: 'number', label: 'Почтовый индекс', restrictions: /^\d{6}$/, errorMessage: 'Индекс должен содержать 6 цифр', showError: false },
-//   {id: 'checked', tag: 'input', class: 'formCheckbox', type: 'checkbox', label: 'Согласен с условиями', required: true, showError: false},
-//   {id: 'message', tag: 'textarea', class: 'formInput', placeholder: 'Введите текст', label: 'Комментарий', maxLength: '300', errorMessage: 'Комментарий может содержать максимум 300 символов', showError: false},
-// ])
-
-// const countries = ['Россия', 'Беларусь', 'Казахстан', 'Китай', 'Индия'];
-// const phoneMask = '+7 (###) ###-##-##';
 
 const formStore = useFormStore();
 
@@ -392,5 +361,5 @@ button[type="submit"]:disabled {
  + Почтовый индекс (6 цифр).
  + Согласие с условиями (обязательное поле, элемент checkbox).
  + Комментарий (до 300 символов).
--  Кнопка «Отправить». Пока отправляется — состояние формы - disabled, показываем пользователю loader.
--  При успехе — экран «Спасибо»; при ошибке — сообщение и кнопка «Повторить». -->
+ + Кнопка «Отправить». Пока отправляется — состояние формы - disabled, показываем пользователю loader.
+ + При успехе — экран «Спасибо»; при ошибке — сообщение и кнопка «Повторить». -->
